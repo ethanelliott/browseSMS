62394a143270b10cbef37222b937100d0e263b8a
Ethan Elliott <ethan.elliott@hotmail.ca>
2020-02-10 22:17:12 -0500

    test

--- src/api/Services/TwilioService.ts
+++ src/api/Services/TwilioService.ts
@@ -13,8 +13,9 @@ export default class TwilioService {
     public async sendMessage(to: string, message: string): Promise<void> {
         const MAX_LENGTH = 100;
         let i = 0;
+        let maxMessage = (message.length / MAX_LENGTH) + 1;
         while (message.length > MAX_LENGTH) {
-            let m = message.substring(0, (MAX_LENGTH) - 1);
+            let m = `[${i}|${maxMessage}]${message.substring(0, (MAX_LENGTH) - 1)}`;
             message = message.substring(MAX_LENGTH);
              let response = await this.client.messages.create({
                 body: m,
