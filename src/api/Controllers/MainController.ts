bdc90fc6c80de16ef569bd85fe243bd685c97d38
Ethan Elliott <ethan.elliott@hotmail.ca>
2020-02-10 21:18:43 -0500

    test

--- src/api/Controllers/MainController.ts
+++ src/api/Controllers/MainController.ts
@@ -17,10 +17,34 @@ export class MainController {
 
     @Get('/receive')
     public receive(
-        @QueryParam('from') fr: string
+        @QueryParam('From') FROM: string,
+        @QueryParam('To') TO: string,
+        @QueryParam('Body') BODY: string,
     ): any {
-        console.log(fr);
+        console.log(TO, FROM, BODY);
         return "HELLO";
     }
 
+    /**
+     * /api/receive?
+     * ToCountry=CA
+     * &ToState=ON
+     * &SmsMessageSid=SMeb4279fbd29a97b59c4162184185c9df
+     * &NumMedia=0
+     * &ToCity=Toronto
+     * &FromZip=
+     * &SmsSid=SMeb4279fbd29a97b59c4162184185c9df
+     * &FromState=ON
+     * &SmsStatus=received
+     * &FromCity=HAMILTON
+     * &Body=HELLO
+     * &FromCountry=CA
+     * &To=%2B16474961070
+     * &ToZip=
+     * &NumSegments=1
+     * &MessageSid=SMeb4279fbd29a97b59c4162184185c9df
+     * &AccountSid=ACd779857453dee735f767c904b566791c
+     * &From=%2B19059757241
+     * &ApiVersion=2010-04-01
+     */
 }
